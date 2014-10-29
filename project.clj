57779d64f3c2a8757f29b303dc924ca2a5c11dd5
Philipp Ullmann <pu@create.at>
2014-10-29 10:57:36 +0100

    Higher lein min version

--- project.clj
+++ project.clj
@@ -7,33 +7,33 @@
   :dependencies [
                  ;; Business
 
-                 [org.clojure/clojure               "1.6.0"]
-                 [environ                           "1.0.0"]
-                 [com.taoensso/timbre               "3.3.1"]
-                 [faker                             "0.2.2"]
-                 [clojure-humanize                  "0.1.0"]
+                 [org.clojure/clojure            "1.6.0"]
+                 [environ                        "1.0.0"]
+                 [com.taoensso/timbre            "3.3.1"]
+                 [faker                          "0.2.2"]
+                 [clojure-humanize               "0.1.0"]
 
                  ;; Database
 
-                 [clojure.jdbc                      "0.3.0"]
-                 [clojure.jdbc/clojure.jdbc-c3p0    "0.3.0"]
-                 [com.h2database/h2                 "1.4.182"]
-                 [yesql                             "0.4.0"]
+                 [clojure.jdbc                   "0.3.0"]
+                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.0"]
+                 [com.h2database/h2              "1.4.182"]
+                 [yesql                          "0.4.0"]
 
                  ;; Web
 
-                 [http-kit                          "2.1.19"]
-                 [liberator                         "0.12.2"]
-                 [ring-cors                         "0.1.4"]
-                 [ring.middleware.logger            "0.5.0"]
-                 [compojure                         "1.2.0"]]
+                 [http-kit                       "2.1.19"]
+                 [liberator                      "0.12.2"]
+                 [ring-cors                      "0.1.4"]
+                 [ring.middleware.logger         "0.5.0"]
+                 [compojure                      "1.2.0"]]
 
   :profiles      {:dev {:dependencies [[ring-mock "0.1.5"]
                                        [midje     "1.6.3"]
                                        [cheshire  "5.3.1"]]}
                   :production {:env {:production true}}}
 
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
   :plugins          [[environ/environ.lein "0.3.1"]]
   :hooks            [environ.leiningen.hooks]
   :main             blog.core
